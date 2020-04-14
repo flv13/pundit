90fff914d58a4ece71670228ddb0c6e919fc385c
Flavian Iordache <flavian.iordache@allcloud.io>
2020-04-14 12:20:15 +0300

    1

--- pipeline-scripts/afterInstall.sh
+++ pipeline-scripts/afterInstall.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rsync --delete-before --verbose --archive /home/temp /home/ubuntu/app/ > /home/ubuntu/deploy.log
+rsync --delete-before --verbose --archive /home/temp/ /home/ubuntu/app/ > /home/ubuntu/deploy.log
